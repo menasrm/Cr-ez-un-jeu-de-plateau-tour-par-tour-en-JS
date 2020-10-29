ac80f9077f62eb3c241b1bb89eb1a950780ea034
Menasra Mohamed <mohamedmenasradu31@outlook.fr>
2020-10-29 18:45:42 +0100

    ajout des premiers fichier du projet

---
+++ exercice.js
@@ -0,0 +1,22 @@
+$('body').css('background-color', 'orange');
+
+
+function afficherTableau() {
+	let body = document.getElementsByTagName("body")[0];
+	let tableau = document.createElement("table");
+	let tableauBody = document.createElement("tbody");
+
+  	for (var i = 0; i < 10; i++) {
+    	var ligne = document.createElement("tr");
+	
+		for (var j = 0; j < 10; j++) {
+      		var cellule = document.createElement("td");
+      		ligne.appendChild(cellule);
+   		}
+   	 	tableauBody.appendChild(ligne);
+	}
+	
+	tableau.appendChild(tableauBody);
+	body.appendChild(tableau);
+}
+afficherTableau();
